ff3d05e9866e2f974d2758123ff4f1b4864cb67d
DAMIEN BRUN <makowildcat@gmail.com>
2015-05-22 14:33:21 +0200

    Add Android Part test communication with Serial

---
+++ Andruino_Simple/Andruino_Simple.ino
@@ -0,0 +1,21 @@
+static int counter = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  Serial.print("Tick #");
+  Serial.print(counter++, DEC);
+  Serial.print("\n");
+
+  if (Serial.peek() != -1) {
+    Serial.print("Read: ");
+    do {
+      Serial.print((char) Serial.read());
+    } while (Serial.peek() != -1);
+    Serial.print("\n");
+  }
+  delay(1000);
+}
